979aedbf4282304a5605a3953cabc81f3dee1faa
ThunderSphun <manderijn2000@gmail.com>
2025-09-10 15:22:53 +0200

    added setup and cleanup for window + imgui

--- main.cpp
+++ main.cpp
@@ -1,5 +1,108 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <imgui.h>
+#include <backends/imgui_impl_opengl3.h>
+#include <backends/imgui_impl_glfw.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <cstdlib>
 #include <iostream>
+#include <format>
+
+GLFWwindow* window = nullptr;
+GLFWcursor* cursor = nullptr;
+
+void init() {
+    glfwSetErrorCallback([](int error, const char* description) {
+		std::cerr << std::format("GLFW Error {}: {}", error, description) << std::endl;
+	});
+
+    if (!glfwInit())
+		exit(-1);
+
+	std::atexit(glfwTerminate);
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+
+	window = glfwCreateWindow(1080, 720, "Minecraft", nullptr, nullptr);
+	if (window == nullptr)
+		exit(-1);
+
+	std::atexit([](){glfwDestroyWindow(window);});
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
+
+	cursor = glfwCreateStandardCursor(GLFW_CROSSHAIR_CURSOR);
+	std::atexit([](){glfwDestroyCursor(cursor);});
+	glfwSetCursor(window, cursor);
+
+	if (!IMGUI_CHECKVERSION())
+		exit(-1);
+
+	ImGui::CreateContext();
+	std::atexit([](){ImGui::DestroyContext();});
+	ImGuiIO& io = ImGui::GetIO();
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;
+	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+	io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
+	io.ConfigViewportsNoTaskBarIcon = true;
+
+	ImGui::StyleColorsDark();
+	ImGuiStyle& style = ImGui::GetStyle();
+
+	if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable) {
+		style.WindowRounding = 0.0f;
+		style.Colors[ImGuiCol_WindowBg].w = 1.0f;
+	}
+
+	if (!ImGui_ImplGlfw_InitForOpenGL(window, true))
+		exit(-1);
+
+	std::atexit(ImGui_ImplGlfw_Shutdown);
+
+	if (!ImGui_ImplOpenGL3_Init("#version 430"))
+		exit(-1);
+
+	std::atexit(ImGui_ImplOpenGL3_Shutdown);
+}
 
 int main() {
-	std::cout << "Hello, World!" << std::endl;
+	init();
+
+	ImGuiIO& io = ImGui::GetIO();
+
+	glm::vec3 bgCol(0.9, 0.9, 1.0f);
+
+	while (!glfwWindowShouldClose(window)) {
+		double time = glfwGetTime();
+		static double pTime = time;
+		glfwPollEvents();
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+		glClearColor(bgCol.r, bgCol.g, bgCol.b, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		ImGui::Begin("Debug menu");
+		ImGui::ColorEdit3("clear color", glm::value_ptr(bgCol));
+		ImGui::Text("frame duration: %f", time - pTime);
+		ImGui::End();
+
+		ImGui::Render();
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+		if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable) {
+			ImGui::UpdatePlatformWindows();
+			ImGui::RenderPlatformWindowsDefault();
+		}
+
+		pTime = time;
+		glfwSwapBuffers(window);
+	}
 }
