bd0e6a1e5bfd14e7e9ce34ef2904d212d4b8d452
ThunderSphun <manderijn2000@gmail.com>
2025-09-26 17:21:34 +0200

    made use of shaderprogram

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@
 
 #include <glm/glm.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/string_cast.hpp>
 
 #include <cstdlib>
 #include <iostream>
@@ -30,7 +31,7 @@ void init() {
 
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
 
 	window = glfwCreateWindow(1080, 720, "Minecraft", nullptr, nullptr);
 	if (window == nullptr)
@@ -90,12 +91,25 @@ int main() {
 	program
 		->attachShader(Minecraft::Assets::Shader::parse(std::filesystem::path("simple"), GL_VERTEX_SHADER))
 		->attachShader(Minecraft::Assets::Shader::parse(std::filesystem::path("simple"), GL_FRAGMENT_SHADER))
+		->bindAttribute(0, "a_position")
+		->bindAttribute(1, "a_color")
 		->link()
 		->use();
 
 	ImGuiIO& io = ImGui::GetIO();
 
 	glm::vec3 bgCol(0.9, 0.9, 1.0f);
+
+	glm::mat4x4 model = glm::mat4x4(1);
+	glm::mat4x4 view = glm::lookAt(glm::vec3(0, 0, -1), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
+	glm::mat4x4 proj = glm::perspective(45.0f, 800 / 600.0f, 0.1f, 100.0f);
+
+	glm::mat4x4 mvp = proj * view * model;
+
+	program->setUniform("mvpMatrix", mvp);
+
+	glEnableVertexAttribArray(0);
+	glEnableVertexAttribArray(1);
 
 	while (!glfwWindowShouldClose(window)) {
 		program->update();
@@ -105,12 +119,28 @@ int main() {
 		double time = glfwGetTime();
 		static double pTime = time;
 
+		program->setUniform("time", (float) time);
+
 		ImGui_ImplOpenGL3_NewFrame();
 		ImGui_ImplGlfw_NewFrame();
 		ImGui::NewFrame();
 
 		glClearColor(bgCol.r, bgCol.g, bgCol.b, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		static glm::vec3 triangleVertices[] = {
+			{glm::cos(0.0f * glm::two_pi<float>() / 3), glm::sin(0.0f * glm::two_pi<float>() / 3), 1.0f},
+			{glm::cos(1.0f * glm::two_pi<float>() / 3), glm::sin(1.0f * glm::two_pi<float>() / 3), 1.0f},
+			{glm::cos(2.0f * glm::two_pi<float>() / 3), glm::sin(2.0f * glm::two_pi<float>() / 3), 1.0f},
+		};
+		static glm::vec3 triangleColors[] = {
+			{1, 0, 0},
+			{0, 1, 0},
+			{0, 0, 1},
+		};
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(glm::vec3), triangleVertices);
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(glm::vec3), triangleColors);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		ImGui::Begin("Debug menu");
 		ImGui::ColorEdit3("clear color", glm::value_ptr(bgCol));
@@ -123,10 +153,10 @@ int main() {
 		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
 		if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable) {
-            GLFWwindow* backup_current_context = glfwGetCurrentContext();
-            ImGui::UpdatePlatformWindows();
-            ImGui::RenderPlatformWindowsDefault();
-            glfwMakeContextCurrent(backup_current_context);
+			GLFWwindow* backup_current_context = glfwGetCurrentContext();
+			ImGui::UpdatePlatformWindows();
+			ImGui::RenderPlatformWindowsDefault();
+			glfwMakeContextCurrent(backup_current_context);
 		}
 
 		pTime = time;
