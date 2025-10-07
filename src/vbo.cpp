4589da5441a4f991607673cf8dfd92f62b2ec18a
ThunderSphun <manderijn2000@gmail.com>
2025-10-07 14:06:23 +0200

    added error checking on vbo and vao code

--- src/vbo.cpp
+++ src/vbo.cpp
@@ -62,6 +62,38 @@ namespace Minecraft::Assets {
 	void VBO::draw(GLenum shape) {
 		bind();
 		glDrawArrays(shape, 0, vertexCount);
+		{
+			GLenum error = glGetError();
+			if (error == GL_INVALID_ENUM) {
+				std::cerr << "Rendershape is not valid, must be one of " <<
+					"'GL_POINTS', 'GL_LINE_STRIP', 'GL_LINE_LOOP', 'GL_LINES', 'GL_TRIANGLE_STRIP', 'GL_TRIANGLE_FAN', 'GL_TRIANGLES', 'GL_PATCHES'";
+				int version[] = { 3, 0 };
+				glGetIntegerv(GL_MAJOR_VERSION, &version[0]);
+				glGetIntegerv(GL_MINOR_VERSION, &version[1]);
+				if ((version[0] == 3 && version[1] >= 2) || version[0] > 3)
+					std::cerr << ", 'GL_LINE_STRIP_ADJACENCY', 'GL_LINES_ADJACENCY', 'GL_TRIANGLE_STRIP_ADJACENCY', 'GL_TRIANGLES_ADJACENCY'";
+
+				std::cerr << std::endl;
+			} else if (error == GL_INVALID_OPERATION) {
+				bool isMapped = false;
+				glGetBufferParameteriv(GL_ARRAY_BUFFER, GL_BUFFER_MAPPED, (int*) &isMapped);
+
+				if (isMapped)
+					std::cerr << "Buffer is currently mapped for data access, and cannot be rendered" << std::endl;
+				else {
+					GLenum geometryShape = 0;
+					GLint currentProgram = 0;
+
+					glGetIntegerv(GL_CURRENT_PROGRAM, &currentProgram);
+					glGetProgramiv(currentProgram, GL_GEOMETRY_INPUT_TYPE, (int*) &geometryShape);
+					if (geometryShape != shape)
+						std::cerr << "A geometry shader is present, and requires a certain rendershape" << std::endl;
+					else
+						std::cerr << "'glDrawArrays' caused 'GL_INVALID_OPERATION' error with unknown cause" << std::endl;
+				}
+			} else if (error != GL_NO_ERROR)
+				std::cerr << "'glDrawArrays' caused unchecked error" << std::endl;
+		}
 		unbind();
 	}
 }
